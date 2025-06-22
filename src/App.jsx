9400dc6f0a4998f3ebca6e4bb578faf99f15ccf5
shubham kumar <shubhamkr77555@gmail.com>
2025-06-22 15:27:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
-      style={{  backgroundImage: "url('https://images4.alphacoders.com/137/thumb-440-1370557.webp')" }} >
+      style={{  backgroundColor:"#596F62" }} >
       <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <form
